18f4072da0c55a9dc352ca56aa8aec33e800f085
Karttunen <Oskari.Karttunen@metropolia.fi>
2017-04-28 12:18:11 +0300

    Add files via upload

--- totalMain.java
+++ totalMain.java
@@ -8,15 +8,26 @@ import lejos.utility.Delay;
 public class totalMain {
 	public static void main(String[] args) {
 		
+		// rnn for movement
+		// commands are: moveForward(), moveBackward(), turnLeft(), steerLeft(),
+		// turnRight(), steerRight(), stawp(), shut()
 		go2 rnn = new go2();
+		
+		// slap for slapping and slap counting
 		slapMotor slap = new slapMotor();
+		
+		// sensor for looking (doesn't work at the moment)
 		EV3IRSensor irSensor = new EV3IRSensor(SensorPort.S1);
 		IRSensor telem = new IRSensor(irSensor);
 		
+		// press ESCAPE on the EV3 to quit
 		while (!Button.ESCAPE.isDown()) {
 			
 			LCD.clear();
+			
+			// TRIES to print the distance, but is currently only showing 0.0
 			LCD.drawString("" + telem.GetDistance(), 0, 0);
+			
 			LCD.drawString("WHAT SHALL I", 0, 2);
 			LCD.drawString("DESTROY THIS TIME?", 0, 3);
 			Delay.msDelay(50);
