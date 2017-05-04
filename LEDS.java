3419e7ffffcb31d263153a62a353419e6a95fcb1
Jebuswastaken <markuvik@metropolia.fi>
2017-05-04 11:19:57 +0300

    Update LEDS.java

--- LEDS.java
+++ LEDS.java
@@ -1,13 +1,15 @@
 import lejos.hardware.Button;
 
 /**
-*Tämä luokka määrittelee ledien toiminnan
-*Luonut Markus Vikholm
+*  Tämä luokka määrittelee ledien toiminnan
+* @version 3.13252356565656	
+* @author Markus Vikholm
 */
 public class LEDS {
 	
 
-	// Ledien toimina etsinnän aikana
+	/** Ledien toimina etsinnän aikana
+	*/
 	public void searchlight(){
 	Button.LEDPattern(6);
 	
@@ -15,19 +17,22 @@ public class LEDS {
 	
 	}
 	
-	//Ledien toiminta kun robotti saavuttaa kohdetta
+	/** Ledien toiminta kun robotti saavuttaa kohdetta
+	*/
 	public void approach(){
 	Button.LEDPattern(9);
 	
 	Button.LEDPattern(7);
 	}
 	
-	//Ledien toiminta kun robotti läimäisee
+	/** Ledien toiminta kun robotti läimäisee
+	*/
 	public void slap(){
 	Button.LEDPattern(9);
 	}
 	
-	//Ledien toiminta shutdownin aikana
+	/** Ledien toiminta shutdownin aikana
+	*/
 	public void shutDown(){
 	Button.LEDPattern(3);
 	Button.LEDPattern(2);
