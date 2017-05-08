b45a64e785309fbaa6be0310448d23cbc315c9ea
Jebuswastaken <markuvik@metropolia.fi>
2017-05-08 10:35:29 +0300

    Update LEDS.java

--- LEDS.java
+++ LEDS.java
@@ -10,8 +10,8 @@ import lejos.hardware.Button;
 public class LEDS {
 	
 
-	/** Ledien toimina etsinn‰n aikana
-	 */
+	// Ledien toimina etsinn√§n aikana
+	 
 	public void searchlight(){
 	Button.LEDPattern(6);
 	
@@ -19,22 +19,22 @@ public class LEDS {
 	
 	}
 	
-	/**Ledien toiminta kun robotti saavuttaa kohdetta
-	 */
+	//Ledien toiminta kun robotti saavuttaa kohdetta
+	 
 	public void approach(){
 	Button.LEDPattern(9);
 	
 	Button.LEDPattern(7);
 	}
 	
-	/**Ledien toiminta kun robotti l‰im‰isee
-	 */
+	///Ledien toiminta kun robotti l√§im√§isee
+	 
 	public void slap(){
 	Button.LEDPattern(9);
 	}
 	
-	/**Ledien toiminta shutdownin aikana
-	 */
+	//Ledien toiminta shutdownin aikana
+	 
 	public void shutDown(){
 	Button.LEDPattern(3);
 	Button.LEDPattern(2);
