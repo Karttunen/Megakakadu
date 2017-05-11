437e0099b9baadc3da4f3411cc3a888ca01201d8
helenasimolin <helena.simolin@metropolia.fi>
2017-05-12 00:06:50 +0300

    Kommentit

--- Noise.java
+++ Noise.java
@@ -1,32 +1,34 @@
 import java.util.Random;
-
 import lejos.hardware.Sound;
-//import lejos.utility.Delay;
 
-/** 
- * ƒ‰ntelee
+/**
+ * Tuottaa ‰‰nisarjan
+ * 
+ * @version 1.0
  * @author Helena
- * */
+ *
+ */
 public class Noise {
 	
 	/**
-	 * Tuottaa satunnaisia ‰‰ni‰ 10 per‰kk‰in
-	 * ƒ‰nten taajuus vaihtelee ..5 kHz
-	 * Kesto vaihtelee 30..100 msek
-	 * @author Helena
+	 * Tuottaa satunnaisia ‰‰ni‰ 10 per‰kk‰in. 
+	 * ƒ‰nten taajuus vaihtelee 1..5 kHz. 
+	 * Kesto vaihtelee 30..200 msek. 
+	 * Volyymi 100 %. 
+	 * 
 	 */
 	public void Mutise() {
 		
-		Random rand = new Random();	//satunnaisesti
+		Random rand = new Random();	//luodaan satunnaisgeneraattori
 		
-		int aFrequency;
-		int aDuration;
-		//int aVolume;
+		int aFrequency;	//‰‰nen taajuus Hertzein‰
+		int aDuration;	//kesto
+		//int aVolume;	//voimakkuus
 		
 		for (int i=0; i<10;i++){
-			aFrequency = rand.nextInt(5000); //normaali ‰‰nialue 1..4 kHz
-			aDuration = rand.nextInt(200);	//msek
-			//aVolume = 30+rand.nextInt(70);	//prosentteina
+			aFrequency = 1000+rand.nextInt(5000); 	// normaali ‰‰nialue on 1..4 kHz
+			aDuration = 30+rand.nextInt(200);		// millisekunteina
+			//aVolume = rand.nextInt(30, 100);		// prosentteina 100 => t‰ysill‰
 			//Sound.playTone(aFrequency, aDuration, aVolume);
 			Sound.playTone(aFrequency, aDuration, 100);
 		}		
