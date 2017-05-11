74b1fcc35c670eaadce3f2465ea6085719385d12
Karttunen <Oskari.Karttunen@metropolia.fi>
2017-05-11 17:56:48 +0300

    Update BumpSensor.java

--- BumpSensor.java
+++ BumpSensor.java
@@ -1,8 +1,12 @@
-
-
 import lejos.hardware.port.SensorPort;
-//import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
+/**
+*
+* Täältä katsotaan onko kosketussensoria painettu vai ei.
+*
+* @version 0.1
+* @author Mikael Rämö
+*/
 
 public class BumpSensor extends Thread{
 	
@@ -14,15 +18,20 @@ public class BumpSensor extends Thread{
 	boolean stopSampling = false;
 	float bumpSample;
 	
-	
+	/** Pysäyttää homman.
+	*/
 	public void stopSampling(){
 		stopSampling = true;
 	}
 	
+	/** Palauttaa painallus tuloksen.
+	*/
 	public float getSample(){
 		return this.bumpSample;
 	}
 	
+	/** Etsii koko ajan painalluksen tulosta.
+	*/
 	public void run () {
 			while (!stopSampling) {
 				float[] sample = new float[ts.sampleSize()];
